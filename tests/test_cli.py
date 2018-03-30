b65d3a18269209ceab19e238c8987616433db86f
Alex Chan <alex@alexwlchan.net>
2018-03-30 10:30:23 +0100

    Use a fixture to create an input file

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,24 +1,37 @@
 # -*- encoding: utf-8
 
+import os
+
 import pytest
 from schema import SchemaError
 
 from itpe import cli
 
 
+@pytest.fixture
+def example_file(tmpdir):
+    curdir = os.path.abspath(os.curdir)
+    try:
+        os.chdir(tmpdir)
+        open('example.csv', 'w').write('')
+        yield
+    finally:
+        os.chdir(curdir)
+
+
 @pytest.mark.parametrize('argv, expected_message', [
     (['--input', 'doesnotexist.csv'],
      "--input='doesnotexist.csv' should be readable"),
 
-    (['--input', 'tox.ini', '--width', '-5'],
+    (['--input', 'example.csv', '--width', '-5'],
      "--width='-5' should be a positive integer"),
-    (['--input', 'tox.ini', '--width', '6xx'],
+    (['--input', 'example.csv', '--width', '6xx'],
      "--width='6xx' should be a positive integer"),
 
-    (['--input', 'tox.ini', '--output', 'tox.ini'],
-     "--output='tox.ini' already exists!"),
+    (['--input', 'example.csv', '--output', 'example.csv'],
+     "--output='example.csv' already exists!"),
 ])
-def test_errors_are_helpful(argv, expected_message):
+def test_errors_are_helpful(example_file, argv, expected_message):
     with pytest.raises(SchemaError) as err:
         cli.get_args(argv)
     assert str(err.value) == expected_message
